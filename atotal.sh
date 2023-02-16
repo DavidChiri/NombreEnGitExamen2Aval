43ee1df7f6210983b473ebc75d89171ee58925d3
David Chirivella <dachga01@floridauniversitaria.es>
2023-02-16 09:32:34 +0100

    Arreglando atotal.sh

--- atotal.sh
+++ atotal.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# Definir la variable que se utilizará
 
-function calcular_consumo {  
-    ciudad=$1  
-    consumo=$(($RANDOM % 5000))
-    echo "El consumo de $ciudad es de $consumo kWh"
-}
+ciudadParametro=$1
+suma_total=0
 
-while true; do
-    read -p "Introduce el nombre de la ciudad: " ciudad
-    if [ "$ciudad" == "Madrid" ] || [ "$ciudad" == "Barcelona" ] || [ "$ciudad" == "Valencia" ]; then
-        calcular_consumo $ciudad
-        break
-    else
-        echo "La ciudad introducida no existe"
-    fi
-done
+while read -r linea; do
+  ciudad=$(echo "$linea" | cut -d ' ' -f 1)
+  mes=$(echo "$linea" | cut -d ' ' -f 2)
+  anyo=$(echo "$linea" | cut -d ' ' -f 3)
+  consumo=$(echo "$linea" | cut -d ' ' -f 4)
+
+ if [ "$ciudadParametro" = "$ciudad" ]; then
+        total=$(expr $total + $consumo)
+       
+  fi
+
+done < consumos.txt
+
+ echo "La suma total es: $total "
